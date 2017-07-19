b4fe59caa7b246b804c0a14b701a6fa637a1bd70
Kevin Law <kevin.uw.law@gmail.com>
2017-07-18 21:35:50 -0700

    almost

--- main.py
+++ main.py
@@ -18,18 +18,24 @@ class Blog(db.Model):
     deleted = db.Column(db.Boolean)
 
 
-    def __init__(self, title):
+    def __init__(self, title, body):
         self.title = title
+        self.body = body
         self.deleted = False
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
 
     return redirect('/blog')
 
 @app.route('/blog', methods =['POST', 'GET'])
 def blog():
+    if request.args:
+        blog_id = request.args.get('id')
+        blog_id = Blog.query.get(blog.id)
+        return redirect('blogPage', blog=blog_id)
+
     total_blogs = Blog.query.filter_by(deleted=False).all()
     deleted_blogs = Blog.query.filter_by(deleted=True).all()
     return render_template('blog.html', title='Build a Blog!', blogs=total_blogs, deletedBlogs= deleted_blogs)
@@ -41,27 +47,33 @@ def newpost():
 
         blog_title = request.form['blogTitle']
         blog_body = request.form['blogBody']
-        new_blog = Blog(blog_title)
-        blog_entry = Blog(blog_body)
-        db.session.add(new_blog)
-        db.session.add(blog_entry)
-        db.session.commit()
+       
 
-        if blog_title == " " or blog_body == " ":
-            title_error = "Successful Title"
-            body_error = "Sucessful Body"
-            if blog_title == " " and blog_body == " ":
-                title_error = "Your blog is missing a title"
-                body_error = "Your blog is missing content"   
-            elif blog_title == " ":
-                title_error= "Your blog is missing a title"
-            elif body_body == " ":
-                body_error= "your blog is missing content"
-            return render_template('newpost.html', title_error = title_error, body_error = body_error)    
+        title_error = ''
+        body_error = ''
+
+        if blog_title == "" and blog_body == "":
+            title_error = "Your blog is missing a title"
+            body_error = "Your blog is missing content"   
+        elif blog_title == "":
+            title_error= "Your blog is missing a title"
+        elif blog_body == "":
+            body_error= "your blog is missing content"
+
+        if body_error or title_error:
+            return render_template('newpost.html', title_error = title_error, body_error = body_error,
+            blogTitle = blog_title,
+            blogBody = blog_body)    
         
-    else:
+        else:
 
-        return render_template('/newpost.html')
+            blog = Blog(blog_title, blog_body)
+            db.session.add(blog)
+            db.session.commit()
+            blogId = blog.id
+            link = "?id=" + str(blogId)
+
+            return redirect('/blog' + link)
 
     return render_template('newpost.html')
 @app.route('/delete-blog', methods=['POST'])
